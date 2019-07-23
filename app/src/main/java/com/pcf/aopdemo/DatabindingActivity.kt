d3fb3e66fae0ff05512f9a2d3df7aba920a823f5
pengchengfuGit <785961112@qq.com>
2019-07-23 09:24:11 +0800

    kotlin

--- app/src/main/java/com/pcf/aopdemo/DatabindingActivity.kt
+++ app/src/main/java/com/pcf/aopdemo/DatabindingActivity.kt
@@ -5,21 +5,21 @@ import android.databinding.ViewDataBinding
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class KotlinActivity : AppCompatActivity(), View.OnClickListener {
+class DatabindingActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var viewDataBinding = DataBindingUtil.setContentView<ViewDataBinding>(this, R.layout.activity_main)
         MyApp.init(this)
-        val button = findViewById<Button>(R.id.button)
+//        val button = findViewById<Button>(R.id.button)
         button.setOnClickListener(this)
     }
 
     @SingleClick
     override fun onClick(p0: View?) {
-        Toast.makeText(this@KotlinActivity, "我被点击了", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this@DatabindingActivity, "我被点击了", Toast.LENGTH_SHORT).show()
     }
 }
